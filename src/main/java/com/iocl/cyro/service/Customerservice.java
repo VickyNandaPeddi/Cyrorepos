4e6abda7c87c5dd036d4eb861c78de14996532ab
NandaKishore.Peddi <vickypeddi9@gmail.com>
2022-02-23 10:32:08 +0530

    email  sending completed

--- src/main/java/com/iocl/cyro/service/Customerservice.java
+++ src/main/java/com/iocl/cyro/service/Customerservice.java
@@ -2,9 +2,13 @@ package com.iocl.cyro.service;
 
 import java.util.List;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.iocl.cyro.dao.Customerdao;
@@ -17,13 +21,35 @@ public class Customerservice {
 
 	@Autowired
 	public Customerdao customerdao;
+	@Autowired
+	private JavaMailSender mailSender;
 
 	public Customer saveCustomer(Customer customer) {
+		Customer customer2 = customerdao.save(customer);
 
-		return customerdao.save(customer);
+		String emailid = customer2.getEmailid();
+		String sendEmailToRegisteredCustomer = sendEmailToRegisteredCustomer(emailid);
+		System.out.println("send email" + sendEmailToRegisteredCustomer);
+		return customer2;
 	}
 
 	public List<Customer> findAllCustomers() {
 		return customerdao.findAll();
+	}
+
+	public String sendEmailToRegisteredCustomer(String to) {
+		MimeMessage message = mailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(message);
+
+		try {
+			helper.setTo(to);
+			helper.setText("Saved Sucessfully");
+			helper.setSubject("Mail From Iocl");
+		} catch (MessagingException e) {
+			e.printStackTrace();
+			return "Error while sending mail ..";
+		}
+		mailSender.send(message);
+		return "Mail Sent Success!";
 	}
 }

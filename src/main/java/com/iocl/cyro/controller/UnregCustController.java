87b786727d2922b706937f115cf59a44c7456bd5
NandaKishore.Peddi <27278>
2022-04-18 10:49:26 +0530

    updated order

--- src/main/java/com/iocl/cyro/controller/UnregCustController.java
+++ src/main/java/com/iocl/cyro/controller/UnregCustController.java
@@ -1,11 +1,6 @@
 package com.iocl.cyro.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +8,7 @@ import com.iocl.cyro.model.ApiResponse;
 import com.iocl.cyro.model.MstProduct;
 
 import javax.servlet.http.HttpServletResponse;
+
 import com.iocl.cyro.model.TrnOrderDetails;
 import com.iocl.cyro.service.MstModelService;
 import com.iocl.cyro.service.TrnOrderDetailsService;
@@ -22,36 +18,41 @@ import lombok.extern.slf4j.Slf4j;
 import java.io.*;
 import java.util.ArrayList;
 
-import org.springframework.web.bind.annotation.RequestBody;
-
 @RestController
-@CrossOrigin(origins = { "http://localhost:4200", "https://uat.indianoil.co.in", "https://spandan.indianoil.co.in" })
+@CrossOrigin(origins = {"http://localhost:4200", "https://uat.indianoil.co.in", "https://spandan.indianoil.co.in"})
 @RequestMapping("/UnregCust")
 @Slf4j
 public class UnregCustController {
 
-	@Autowired
-	private TrnOrderDetailsService trnOrderDetailsService;
+    @Autowired
+    private TrnOrderDetailsService trnOrderDetailsService;
 
-	@PostMapping("/createOrder")
-	public ResponseEntity<ApiResponse> createOrder(@RequestBody TrnOrderDetails trnOrderDet) {
-		TrnOrderDetails createOrder = trnOrderDetailsService.createOrder(trnOrderDet);
-		System.out.print("inside controller 123 " + trnOrderDet.getCustName());
-		log.error("created order" + createOrder.toString());
-		return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ApiResponse(true, "Details saved successfully"));
+    @PostMapping("/createOrder")
+    public ResponseEntity<ApiResponse> createOrder(@RequestBody TrnOrderDetails trnOrderDet) {
+        TrnOrderDetails createOrder = trnOrderDetailsService.createOrder(trnOrderDet);
+        System.out.print("inside controller 123 " + trnOrderDet.getCustName());
+//		log.error("created order" + createOrder.toString());
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ApiResponse(true, "Details saved successfully"));
 //		return trnOrderDet.getCity();
 
-	}
+    }
 
-	@GetMapping("/order-list")
-	public ArrayList<TrnOrderDetails> fetchOrderDetails() {
-		System.out.println("ORDER LIST");
-		return trnOrderDetailsService.fetchAllOrders();
-	}
+    @GetMapping("/order-list")
+    public ArrayList<TrnOrderDetails> fetchOrderDetails() {
+        System.out.println("ORDER LIST");
+        return trnOrderDetailsService.fetchAllOrders();
+    }
 
-	@GetMapping("/order-list/{reqno}")
-	public TrnOrderDetails fetchOrderDetails(@PathVariable("reqno") int reqno) {
-		System.out.println("ORDER LIST");
-		return trnOrderDetailsService.fetchOneOrders(reqno);
-	}
+    @GetMapping("/order-list/{reqno}")
+    public TrnOrderDetails fetchOrderDetails(@PathVariable("reqno") int reqno) {
+        System.out.println("ORDER LIST");
+        return trnOrderDetailsService.fetchOneOrders(reqno);
+    }
+
+
+    @PutMapping("/update/{reqno}")
+    public ResponseEntity<ApiResponse> updateTrnorderdetails(@PathVariable("reqno") int id, @RequestBody TrnOrderDetails trnOrderDetails) {
+        TrnOrderDetails orderDetails = trnOrderDetailsService.updateTrnOrderdetails(id, trnOrderDetails);
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(new ApiResponse(true, "Details Updated successfully"));
+    }
 }
